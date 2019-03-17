831cf8333a37fc404a90a2d3d533411ae57cbe88
Kimberly Rouse <ksrouse94@gmail.com>
2019-03-17 01:00:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
         words.each do |word|
           word.capitalize!
         end 
-        event = words.join
+        event = words.join(" ")
       else 
         event = holiday.to_s.capitalize
       end  
